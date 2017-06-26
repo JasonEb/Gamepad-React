9e3bc744805edd60f884909590da56c610de66d9
holyoke <jasonebueng@gmail.com>
2017-06-25 19:48:59 -0700

    enables routes

--- components/root.jsx
+++ components/root.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, Route, hashHistory } from 'react-router'
+import { HashRouter, Route } from 'react-router-dom'
 
 import App from './gamecube_controller/app.jsx'
 
 class Root extends React.Component {
   render () {
-    <Router history={hashHistory}>
-      <Route path="/" component={App} />
-    </Router>
+    return (
+      <HashRouter>
+        <Route path="/debug" component={App} />
+      </HashRouter>
+    )
   }
 }
 
